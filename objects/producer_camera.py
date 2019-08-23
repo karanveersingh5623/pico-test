70de72a0701c0e4c1ce1c8402f596cb3250bf268
Avinash Bendigeri <abendigeri@gmail.com>
2019-08-23 13:50:15 +0530

    Added the Producer Code

--- objects/producer_camera.py
+++ objects/producer_camera.py
@@ -96,10 +96,58 @@ def publish_camera():
 
 
 # In[19]:
+def publish_video(video_file):
+    """
+    Publish given video file to a specified Kafka topic. 
+    Kafka Server is expected to be running on the localhost. Not partitioned.
+    
+    :param video_file: path to video file <string>
+    """
+    # Start up producer
+    producer = KafkaProducer(bootstrap_servers=brokers,
+                            value_serializer=lambda v: json.dumps(v).encode('utf-8'))
+
+    # Open file
+    video = cv2.VideoCapture(video_file)
+    
+    print('publishing video...')
+    
+    framecount = 0 
+
+    while(video.isOpened()):
+        success, frame = video.read()
+
+        # Ensure file was read successfully
+        if not success:
+            print("bad read!")
+            break
+        
+        # Convert image to png
+        ret, buffer = cv2.imencode('.jpg', frame)
+        
+        utc_dt = pytz.utc.localize(datetime.datetime.now())
+        now_ts_utc = (utc_dt - datetime.datetime(1970, 1, 1, tzinfo=pytz.utc)).total_seconds()
+
+        camera_data['image_bytes'] = base64.b64encode(buffer).decode('utf-8')
+
+        camera_data['frame_count'] = str(framecount)
+
+        camera_data['capture_time'] = str(now_ts_utc)
+        
+        
+        
+
+        # Convert to bytes and send to kafka
+        producer.send(topic, camera_data)
+
+        framecount = framecount + 1
+        time.sleep(0.2)
+    video.release()
+    print('publish complete')
 
 
 if __name__ == "__main__":
-    publish_camera()
+    publish_video()
 
 
 # In[ ]:
